b5bbb408c9f1ff723ab18458161ff185349839eb
hb6700 <hb6700@naver.com>
2024-01-02 22:51:25 +0900

    240102

--- src/Q10798.java
+++ src/Q10798.java
@@ -6,11 +6,20 @@ public class Q10798 {
         Scanner scan = new Scanner(System.in);
 
         //총 다섯줄의 입력이 주어진다. 각 줄에는 최소 1개, 최대 15개의 글자들이 빈칸 없이 연속으로 주어진다.
-        String[][] text = new String[5][15];
+        char[][] text = new char[5][15];
 
         String input = "";
         for (int i=0 ; i<text.length ; i++){
             input = scan.nextLine();
+            for(int j = 0; j < input.length(); j++){
+                text[i][j] = input.charAt(j);
+            }
+        }
+
+        for(int j=0; j < input.length(); j++){
+            for (int i=0; i < text.length; i++){
+                System.out.print(text[i][j]);
+            }
         }
     }
 }
---
+++ src/W5Q2798.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class W5Q2798 {
+}
