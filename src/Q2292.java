cc9fda7d95399510b1e861c46258a53bde32c5b4
hb6700 <hb6700@naver.com>
2024-01-13 20:13:56 +0900

    240113

--- src/Q2292.java
+++ src/Q2292.java
@@ -1,58 +1,26 @@
-////import java.util.Scanner;
-////
-////public class Q2292 {
-////    public static void main(String[] args) {
-////        Scanner scan = new Scanner(System.in);
-////        int roomNum = scan.nextInt();
-////
-////        //1 -> 6 -> 12 -> 18 -> 24
-////        //2 ~ 7 = 2
-////        //8 ~ 19 = 3
-////        //20 ~ 37 = 4
-////        //38 ~ 61 = 5
-////        //62 ~
-////
-////        //int plural = 2;
-////
-////        int route;
-////
-////        if(roomNum == 1){
-////            System.out.print(1);
-////        }else{
-////
-////            System.out.print(0);
-////        }
-////    }
-////}
-////
-////
-////package bronze;
-////
-////        import java.io.BufferedReader;
-////        import java.io.IOException;
-////        import java.io.InputStreamReader;
-//
-//// 2292 벌집 브론즈2
-//public class Q2292 {
-//    public static void main(String[] args) throws IOException {
-//
-//
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//
-//        int N = Integer.parseInt(br.readLine());
-//
-//        int count = 1; // 거리
-//        int range = 2; // 범위
-//
-//        if (N == 1) {
-//            System.out.println(1);
-//        } else {
-//            while (range <= N) {
-//                range = range + (6 * count);
-//                count++;
-//            }
-//            System.out.println(count);
-//        }
-//    }
-//}
-//
+import java.util.Scanner;
+
+public class Q2292{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+        int count = 1;      //출력 : 지나간 방의 개수
+        int room = 2;       //방 번호
+        if(input == 1){
+            System.out.println(count);
+        }else {
+            while(room <= input){
+                room = room + (6*count);
+                count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
+
+//1 -> 6 -> 12 -> 18 -> 24
+//2 ~ 7 = 2(6칸)
+//8 ~ 19 = 3(12칸)
+//20 ~ 37 = 4(18칸)
+//38 ~ 61 = 5(24칸)
+//62 ~
--- src/Q2839.java
+++ src/Q2839.java
@@ -5,11 +5,18 @@ public class Q2839 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int kilo = scan.nextInt();
+        int cnt = 0;
 
-        if (kilo - 5 < 0) {
-            System.out.println(-1);
-        } else {
-            System.out.println(kilo/5+1);
+        while (true){
+            if(kilo % 5 == 0){      //5로 나누어 떨어질 때
+                System.out.println(kilo/5 + cnt);
+                break;
+            } else if (kilo < 0) {  //입력값이 0보다 작을 경우
+                System.out.println(-1);
+                break;
+            }
+            kilo = kilo-3;
+            cnt++;
         }
     }
 }
