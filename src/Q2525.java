63ee62ad66acac481766b09ebfb6081e7969681f
hb6700 <hb6700@naver.com>
2024-01-27 23:28:13 +0900

    240127

--- src/Q2525.java
+++ src/Q2525.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Q2525 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2525 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(br.readLine());
+
+        int D = B + C;
+        if(D >= 60){
+            A = A + (D/60);
+            B = D % 60;
+            if(A >= 24){
+                A = A - 24;
+                System.out.println(A + " " + B);
+            }else {
+                System.out.println(A + " " + B);
+            }
+        }else {
+            B = B + C;
+            System.out.println(A + " " + B);
+        }
+    }
 }
--- src/Q2739.java
+++ src/Q2739.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Q2739 {
+import java.util.Scanner;
+
+public class Q2739 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        for(int i=1; i<=9 ; i++){
+            System.out.println(N + " * " + i + " = " + (N*i));
+        }
+    }
 }
