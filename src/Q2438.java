4e4ddc1f6d238658e135b415f37209ef0636afef
hb6700 <hb6700@naver.com>
2024-02-07 21:14:09 +0900

    240207

---
+++ src/Q2438.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q2438 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        for (int i=0 ; i<N ; i++){
+            for (int j=0 ; j<=i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
