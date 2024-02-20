87a059db05e3d41b0468228a998f0fe5866ff553
hb6700 <hb6700@naver.com>
2024-02-20 23:40:49 +0900

    240220

---
+++ src/Q10809.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10809 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        int[] arr = new int[26];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        for(int i = 0; i < S.length(); i++) {
+            char ch = S.charAt(i);
+            if(arr[ch - 'a'] == -1) {	// arr 원소 값이 -1 인 경우에만 초기화
+                arr[ch - 'a'] = i;
+            }
+        }
+
+        for(int val : arr) {	// 배열 출력
+            System.out.print(val + " ");
+        }
+    }
+}
