3fd935b9d1ec03dd1a135678412e6d1ee065238e
hb6700 <hb6700@naver.com>
2024-02-24 22:50:36 +0900

    240224

--- src/Q10951.java
+++ src/Q10951.java
@@ -1,5 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Q10951 {
     public static void main(String[] args) {
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            String str = "";
+            while ((str=br.readLine()) != null){
+                StringTokenizer st = new StringTokenizer(str);
+                if (st.hasMoreTokens()){
+                    bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+                }else {
+                    break;
+                }
+            }
+            br.close();
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
