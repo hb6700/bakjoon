306879f869e8d11de7b4fa24ab6b09a8d42e512c
hb6700 <hb6700@naver.com>
2024-03-05 23:19:46 +0900

    240305_modify(correct)

--- src/Q2581_1.java
+++ src/Q2581_1.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Q2851_1 {
+public class Q2581_1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int M = Integer.parseInt(br.readLine());
