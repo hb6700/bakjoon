ef5d2f1eb762d39c913f97324f165e4bc821631d
hb6700 <hb6700@naver.com>
2024-03-06 21:50:28 +0900

    240306

---
+++ src/Q19532.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q19532 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        bw.write((c*e- b*f) / (a*e - b*d) + " "); //x축
+        bw.write((c*d - a*f) / (b*d - a*e) +""); // y축
+        bw.flush();
+        bw.close();
+
+    }
+}
