d69e0602f1d676d34eac0676b3dbcfc0e99f2478
hb6700 <hb6700@naver.com>
2024-03-20 22:19:15 +0900

    240320

---
+++ src/Q11650.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[][] arr = new Integer[n][2];
+        for (int i=0 ; i<n ; i++){
+             StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (o1, o2) -> (o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]));
+        // Arryas.sort(2차원 배열, (o1,o2) -> ( 조건문 ? true 일때 정렬순서 : false일때 정렬순서));
+
+        for (int i=0 ; i<n ; i++){
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
