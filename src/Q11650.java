fc1ee7fd5024a199c1b65d53186e578beef088ae
hb6700 <hb6700@naver.com>
2024-03-20 22:29:05 +0900

    240320

--- src/Q11650.java
+++ src/Q11650.java
@@ -1,22 +1,22 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class Q11650 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Integer[][] arr = new Integer[n][2];
+        int[][] arr = new int[n][2];
         for (int i=0 ; i<n ; i++){
              StringTokenizer st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, (o1, o2) -> (o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]));
-        // Arryas.sort(2차원 배열, (o1,o2) -> ( 조건문 ? true 일때 정렬순서 : false일때 정렬순서));
+        Arrays.sort(arr, (e1, e2) -> (e1[0] == e2[0] ? e1[1] - e2[1] : e1[0] - e2[0]));
+        // Arrays.sort(2차원 배열, (o1,o2) -> ( 조건문 ? true 일때 정렬순서 : false일때 정렬순서));
 
         for (int i=0 ; i<n ; i++){
             System.out.println(arr[i][0] + " " + arr[i][1]);
